6b935155efaae77ffea1cb16158094c48a8143da
Your Name <you@example.com>
2018-06-26 01:41:58 -0500

    ssl commit

---
+++ src/main/java/com/springboot/demo/SpringBootWebSslAppApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSslAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSslAppApplication.class, args);
+	}
+}
